c27625ec8f2be05dc1dde5a197e4883e18bea751
sachin_sharma <bharatshharma54076@gmail.com>
2026-08-24 18:24:07 +0530

    Fixed Admin CSS using build_files.sh and WhiteNoise

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
-echo "Building the project..."
+#!/bin/bash
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput --clear
-echo "Make Migration..."
-python3.12 manage.py make-migrations --noinput
-python3.12 manage.py migrate --noinput
-echo "BUILD END"
