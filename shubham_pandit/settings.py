a212643586c938052daa811700351f0cbb26161e
sachin_sharma <bharatsharma54076@gmail.com>
2026-08-25 17:27:10 +0530

    Fixed Google verification redirect issue

--- shubham_pandit/settings.py
+++ shubham_pandit/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'shubham_pandit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'template'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
