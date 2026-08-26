b4252fd3ef160b2a30413b7ce573340c9b30972c
sachin_sharma <bharatsharma54076@gmail.com>
2026-08-26 13:13:21 +0530

    Fix pymysql init binding and requirements

--- shubham_pandit/urls.py
+++ shubham_pandit/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, reverse
 from shubham_pandit import views
 from django.conf import settings
 from django.conf.urls.static import static
